5507b9eeafbf5f9a14c75ed09f1eadcd9b143703
rohitzeptonow <rohit.kashyap@zeptonow.com>
2025-04-16 16:09:07 +0530

    Update package exports to include Typeahead component and set it as default export in index.tsx

--- src/package/index.tsx
+++ src/package/index.tsx
@@ -1,7 +1,9 @@
-export { Typeahead } from "./components/Typeahead";
 export type {
   ZeptoTypeAhead,
   ZeptoTypeAheadOption,
   TypeaheadOptionProps,
   ZeptoTypeAheadHeaderProps,
 } from "./types";
+
+import { Typeahead } from "./components/Typeahead";
+export default Typeahead;
